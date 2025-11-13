4a0568f9e1ba04cfb5417c386f68d784b95a3c96
itajacy TI02 <itajacy@bsd.com.br>
2025-11-13 16:31:39 -0300

    aula 6 - Classe vs  Objeto

---
+++ src/basico/ClasseVsObjetoAula5.java
@@ -0,0 +1,31 @@
+package basico;
+
+public class ClasseVsObjetoAula5 {
+}
+// organizar  int, boolean    |          3 , 7.34 , false
+// Estruturas de Dados        vs        Dados
+
+
+//  CLASSES VS OBJETOS
+/*
+
+    Classes representam as Estruturas de Dados
+    Objetos são os valores que obedecem as estruturas de dados
+
+    Classe                  |       Objeto
+
+    Medico
+      atributos:            |   new Medico(id=1834
+        - id                |               nome= Nicolas
+        - nome              |               crm = 1234
+        - crm               |               salario= 6000)
+        - salario           |
+
+                            |   new Medico(id=5854
+                            |               nome= Nulvias
+                            |               crm = 4564
+                            |               salario= 5000)
+                            |
+
+
+ */

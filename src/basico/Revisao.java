b05b0f36739d9d3be2cc38e66c4fa43c5afefd93
itajacy TI02 <itajacy@bsd.com.br>
2025-11-13 15:57:41 -0300

    aula 4 - revisão

--- src/basico/Revisao.java
+++ src/basico/Revisao.java
@@ -1,9 +1,12 @@
 package basico;
 
+// A classe deve ter o mesmo nome do arquivo
 public class Revisao {
-
+// dentro da classe, é onde os métodos são escritos
     // Método Main - Porta de entrada em um APP Java
+
     static void main() {
+
         System.out.println("Revisão!!!");
     }
 }

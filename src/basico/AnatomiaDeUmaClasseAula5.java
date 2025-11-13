23c83e2f910581f1e07311a4583ddd27385f63d1
itajacy TI02 <itajacy@bsd.com.br>
2025-11-13 16:06:57 -0300

    aula 5 - Anatomia de uma classe

---
+++ src/basico/AnatomiaDeUmaClasseAula5.java
@@ -0,0 +1,26 @@
+package basico;
+
+public class AnatomiaDeUmaClasseAula5 {
+}
+
+/*
+
+        Anatomia da Classe
+NomeClasse ==  Responsabilidade
+
+        public class NomeClasse {
+
+            DADOS +  AÇÕES/COMPORTAMENTOS
+
+            Dados = int, double, String
+            Ações/Comportamentos = métodos <= implementar algoritmo
+
+        {
+
+ */
+
+
+/*
+
+App Java =  dividir em conjunto de classes trabalhando de forma colaborativa
+ */

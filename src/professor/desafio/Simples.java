56035da59f0738974787168719a4d8f463eb08af
itajacy TI02 <itajacy@bsd.com.br>
2025-11-13 15:38:05 -0300

    importacao dos arquivos feitos pelo professor

---
+++ src/professor/desafio/Simples.java
@@ -0,0 +1,19 @@
+package desafio;
+
+public class Simples {
+
+    int a = 10;
+    static int b = 12;
+
+    static void main() {
+        // Imprimir o valor da variável a
+        Simples s = new Simples();
+        s.a = 100;
+
+        System.out.println(new Simples().a);
+        System.out.println(s.a);
+
+        System.out.println(b);
+        System.out.println(Simples.b);
+    }
+}

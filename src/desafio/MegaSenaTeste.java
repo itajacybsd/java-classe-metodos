8b3a2ca963f0de27fc66322b62da8979a77de9a9
ita Dev <itajacy@gmail.com>
2025-11-14 13:09:01 -0300

    aula 19 - Desafio Megasena

---
+++ src/desafio/MegaSenaTeste.java
@@ -0,0 +1,11 @@
+package desafio;
+
+public class MegaSenaTeste {
+    static void main() {
+        MegaSena aposta1 = new MegaSena();
+        System.out.println(aposta1);
+
+        MegaSena aposta2 = new MegaSena(3, 9, 23, 32, 47, 59);
+        System.out.println(aposta2);
+    }
+}

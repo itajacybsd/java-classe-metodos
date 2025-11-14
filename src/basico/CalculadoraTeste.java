749361fc818432de8a86ff6c1620b00eddb485b0
ita Dev <itajacy@gmail.com>
2025-11-14 10:09:45 -0300

    aula 15 - Explorando metodos

--- src/basico/CalculadoraTeste.java
+++ src/basico/CalculadoraTeste.java
@@ -6,17 +6,17 @@ public class CalculadoraTeste {
         Calculadora calc = new Calculadora();
 
         double r = calc.somar(3, 7.0);
-        IO.println(r);
-        IO.println(calc.memoria);
+        System.out.println(r);
+        System.out.println(calc.memoria);
 
         calc.limparMemoria();
 
         r = calc.somar(100);
-        IO.println(r);
-        IO.println(calc.memoria);
+        System.out.println(r);
+        System.out.println(calc.memoria);
 
         r = calc.memoriaAoQuadrado();
-        IO.println(r);
-        IO.println(calc.memoria);
+        System.out.println(r);
+        System.out.println(calc.memoria);
     }
 }
